3c2b62d54fdc007ddabc24c2e22df090afaeaccb
Marco S. Nobile <marco.nobile@unive.it>
2025-05-07 12:55:06 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'fst-pso',
   packages = ['fstpso'], 
-  version = '1.8.1',
+  version = '1.9.0',
   description = 'Fuzzy Self-Tuning PSO global optimization library',
   author = 'Marco S. Nobile',
-  author_email = 'nobile@disco.unimib.it',
+  author_email = 'marco.nobile@unive.it',
   url = 'https://github.com/aresio/fst-pso', # use the URL to the github repo
-  keywords = ['fuzzy logic', 'optimization', 'discrete optimization', 'continuous optimization', 'particle swarm optimization',  'pso', 'fst-pso', 'fuzzy self-tuning pso'], # arbitrary keywords
+  keywords = ['fuzzy logic', 'optimization', 'discrete optimization', 'continuous optimization', 'particle swarm optimization',  'pso', 'fst-pso', 'fuzzy self-tuning pso', 'fuzzy time-travel pso'], # arbitrary keywords
   license='LICENSE.txt',
   long_description=open('README.txt', encoding='utf-8').read(),
   classifiers = ['Programming Language :: Python :: 3.7'],
